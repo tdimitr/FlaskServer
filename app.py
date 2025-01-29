a1021216529f18a29c33759358569945d54f6a6b
Dimitris Tsapalas <dim_tsap@outlook.com>
2025-01-29 16:59:01 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+import cloudinary
+from routes import api_bp  
+
+load_dotenv()
+
+cloudinary.config(
+    cloud_name=os.getenv("CLOUD_NAME"),
+    api_key=os.getenv("API_KEY"),
+    api_secret=os.getenv("API_SECRET")
+)
+
+app = Flask(__name__)
+CORS(app)
+
+app.register_blueprint(api_bp)
+
+# This is not needed for deployment with Gunicorn:
+if __name__ == "__main__":
+    from os import environ
+    app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
